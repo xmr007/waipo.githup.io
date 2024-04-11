64d47f775a951da25647a611edce2c0b485f099d
“bombg” <bombtrow714@outlook.com>
2024-04-11 18:04:35 +0800

    GAME
    waipo H5

---
+++ src/settings.ab643.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/StartScene.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",resources:"e0b78",main:"845f1"}};
